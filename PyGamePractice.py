28c8483f9bc3c08775ebfd3706c5e09b159d127d
honestyjohnson <90330004+honestyjohnson@users.noreply.github.com>
2021-11-29 09:31:57 -0500

    v0.7
    draw an ellipse

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,4 +1,4 @@
-# PyGame Practice, Honesty Johnson, 11/29/21 9:26am, v0.6
+# PyGame Practice, Honesty Johnson, 11/29/21 9:31am, v0.7
 
 import pygame, sys
 from pygame.locals import *
@@ -36,3 +36,9 @@ pygame.draw.polygon(windowSurface, GREEN, (( 146, 0), (291, 106), (236, 277), (5
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4) 
 pygame.draw.line(windowSurface, BLUE, ( 120, 60), (60, 120))
 pygame.draw.line(windowSurface, BLUE, ( 60, 120), (120, 120), 4) 
+
+# Draw a circle.
+pygame.draw.circle( windowSurface, BLUE, (300, 50), 20, 0)
+
+# Draw an ellipse.
+pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
