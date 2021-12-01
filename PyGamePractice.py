6dbefa6cfff92656e4704a7a158381d159167726
honestyjohnson <90330004+honestyjohnson@users.noreply.github.com>
2021-12-01 08:51:45 -0500

    v0.9
    finished in the same order

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,4 +1,4 @@
-# PyGame Practice, Honesty Johnson, 12/1/21 8:41am, v0.8
+# PyGame Practice, Honesty Johnson, 12/1/21 8:51am, v0.9
 
 import pygame, sys
 from pygame.locals import *
@@ -27,11 +27,15 @@ textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery 
 
+
 # Draw background onto window surface.
 windowSurface.fill(BABYBLUE) 
 
+
+
 #Draw a green polygon onto the surface.
 pygame.draw.polygon(windowSurface, GREEN, (( 146, 0), (291, 106), (236, 277), (56, 277), (0, 106))) 
+
 
 #Draw blue lines on the windowSurface.
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4) 
@@ -39,19 +43,35 @@ pygame.draw.line(windowSurface, BLUE, ( 120, 60), (60, 120))
 pygame.draw.line(windowSurface, BLUE, ( 60, 120), (120, 120), 4) 
 
 
+
 # Draw a circle.
 pygame.draw.circle( windowSurface, BLUE, (300, 50), 20, 0)
+
 # Draw an ellipse.
 pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 
+
 # Draw text background retangle no surface.
 pygame.draw.rect(windowSurface, RED, (textRect.left -20, textRect.top -20, textRect.width +40, textRect.height +40)) 
+
 
 # Get a pixel array of the surface 
 pixArray = pygame.PixelArray(windowSurface)
 pixArray[480][380] = BLACK
 del pixArray 
 
+
 # Draw the text onto the surface 
 windowSurface.blit(text, textRect) 
 
+
+# Draw the window onto the screen.
+pygame.display.update()
+
+
+# Run the game loop. 
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit() 
