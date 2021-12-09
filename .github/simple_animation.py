9bced177516654b83dc4339f6193d8bd97fdb0a2
honestyjohnson <90330004+honestyjohnson@users.noreply.github.com>
2021-12-09 09:22:11 -0500

    v0.6
    Filled in our windowSurface.
    
    Adjust MOVESPEED of boxes based on location.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Honesty Johnson, 12/09/21, 9:10AM, v0.5
+# Simple Animation with PyGame, Honesty Johnson, 12/09/21, 9:21AM, v0.6
 
 import pygame, sys, time
 from pygame.locals import *
@@ -40,3 +40,21 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    windowSurface.fill(WHITE)
+
+    for b in boxes:
+        # Move the box data structure.
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top += MOVESPEED
+            if b['dir'] == DONRIGHT:
+                b['rect'].left += MOVESPEED
+                b['rect'].top += MOVESPEED
+            if b['dir'] == UPLEFT:
+                b['rect'].left -= MOVESPEED
+                b['rect'].top -= MOVESPEED
+            if b['dir'] == UPRIGHT:
+                b['rect'].left += MOVESPEED
+                b['rect'].top -= MOVESPEED
+
