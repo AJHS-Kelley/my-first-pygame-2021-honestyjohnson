7de06d9dc98d21a6d960ec6e4d83efe08835d25c
honestyjohnson <90330004+honestyjohnson@users.noreply.github.com>
2022-01-19 09:24:08 -0500

    v0.6
    Started the game loop.
    
    Created code to check for arrow keys / WASD movement.

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,22 +1,63 @@
-# Simple Animation with PyGame, Honesty Johnson, 12/09/21, 9:21AM, v0.6
+# Simple Animation with PyGame, Honesty Johnson, 1/19/21, 9:22AM, v0.6
 
 import pygame, sys, time
 from pygame.locals import *
 
 # Setup Pygame
 pygame.init() 
+mainClock = pygame.time.Clock() 
 
-# Setup the Window 
+# Setup the Pygame Window 
 WINDOWWIDTH = 400 
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT),0, 32)
 pygame.display.set_caption('Animation Example!')
 
-# Setup the direction variables. 
-DOWNLEFT = 'downleft'
-DOWNRIGHT = 'downright'
-UPLEFT = 'upleft'
-UPRIGHT = 'upright'
+# Setup colors
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255) 
+
+# Setup the player and food data structures.
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+player= pygame.Rect( 300, 100, 50, 50)
+foods = []
+
+for i in range (20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
+#Movement Variables
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False 
+
+MOVESPEED = 6
+
+# Run the game loop.
+while True:
+    # Check for events. 
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            # Change the keyboard variables.
+            if event.key == K_LEFT or event.key == K_a:
+                moveRight = False 
+                moveLeft = True 
+            if event.key == K_Right or event.key == K_d: 
+                moveLeft = False
+                moveRight = True 
+            if event.key == K_UP or event.key == K_w:
+                movedown = False 
+                moveUp = True 
+            if event.key == K_DOWN or event.key == K_s:
+                moveUp = False 
+                moveDown = True 
+
 
 MOVESPEED = 4
 
@@ -44,7 +85,7 @@ while True:
     windowSurface.fill(WHITE)
 
     for b in boxes:
-        # Move the box data structure.
+        # Change the keyboard variable 
         if b['dir'] == DOWNLEFT:
             b['rect'].left -= MOVESPEED
             b['rect'].top += MOVESPEED
